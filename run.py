76363c6a0ced2c18394f60a7d98a70f69c5c4776
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-02-19 10:05:53 +0000

    re write get hangman function

--- run.py
+++ run.py
@@ -3,13 +3,61 @@ words = ["announce", "size", "amusement", "safe", "dynamic", "shaky"]
 
 def get_randomword():
     """
-    the function gets randon words from the word list
+    the function gets random words from the word list
     """
     random_word = random.choice(words)
 
     print(random_word.upper())
 
-get_randomword()
+
+def get_hangman(incorrect_answer):
+    """
+    the function will print hangman for incorrect answers    
+    """
+    if incorrect_answer == 0:
+        print("\n+===+")
+        print("      |")
+        print("      |")
+        print("      |")
+        print("   ===+")
+    if incorrect_answer == 1:
+        print("\n+===+")
+        print(" O    |")
+        print("      |")
+        print("      |")
+        print("   ===+")
+    if incorrect_answer == 2:
+        print("\n+===+")
+        print(" O    |")
+        print(" |    |")
+        print("      |")
+        print("   ===+")
+    if incorrect_answer == 3:
+        print("\n+===+")
+        print(" O    |")
+        print("/|    |")
+        print("      |")
+        print("   ===+")
+    if incorrect_answer == 4:
+        print("\n+===+")
+        print(" O    |")
+        print("/|\   |")
+        print("      |")
+        print("   ===+")
+    if incorrect_answer == 5:
+        print("\n+===+")
+        print(" O    |")
+        print("/|\   |")
+        print("/     |")
+        print("   ===+")
+    if incorrect_answer == 6:
+        print("\n+===+")
+        print(" O    |")
+        print("/|\   |")
+        print("/ \   |")
+        print("   ===+")
+
+get_hangman(incorrect_answer)
 
 
 
