5fb02deefaf6b8737f76076a818da5574f2957e8
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-02-19 09:54:01 +0000

    rewrite the get random function

--- run.py
+++ run.py
@@ -1,88 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import random
+words = ["announce", "size", "amusement", "safe", "dynamic", "shaky"]
 
-words = ["near", "announce", "size", "amusement", "safe", "dynamic", "shaky", "boast", "pass", "ancient", "enormous", "shake", "wine", "skinny", "elephant", "glass", "travel", "phone", "reject", "emotion", "stress", "marriage", "calculate", "decide", "grumpy", "foolish", "inexpensive", "squirrel"]
-
-# print(random.choice(words))   tried to get a random word from the list
-
-print("Welcome to Hangman game")
-print("___________________________")
-
-def get_random_word():
+def get_randomword():
     """
-    this function gets random word from the list
-    and allow user to select words until there is _ or blank space
+    the function gets randon words from the word list
     """
-    random_Word = random.choice(words)     #it gets random words from the list of words
-    global = random_Word      #using global to make this local variable, a global one
-    while "_" in random_Word or " " in random_Word:  
-        random_Word = random.choice(words)
-    
-    return random_Word.upper()
+    random_word = random.choice(words)
+
+    print(random_word.upper())
+
+get_randomword()
 
 
-def get_hangman():
-    """
-    It prints the hangman when user selects wrong character from the word
-    """
-    if(guess_the_word == 0):
-        print("\n+---+")
-        print("     |")
-        print("     |")
-        print("     |")
-        print("   ===")
-    elif(guess_the_word == 1):
-        print("\n+---+")
-        print(" O    |")
-        print("      |")
-        print("      |")
-        print("   ===")
-    elif(guess_the_word == 2):
-        print("\n+---+")
-        print(" O    |")
-        print(" |    |")
-        print("      |")
-        print("   ===")
-    elif(guess_the_word == 3):
-        print("\n+---+")
-        print(" O    |")
-        print(" |\   |")
-        print("      |")
-        print("   ===")
-    elif(guess_the_word == 4):
-        print("\n+---+")
-        print(" O    |")
-        print("/|\   |")
-        print("      |")
-        print("   ===")
-    elif(guess_the_word == 5):
-        print("\n+---+")
-        print(" O    |")
-        print("/|\   |")
-        print("  \   |")
-        print("   ===")
-    elif(guess_the_word == 6):
-        print("\n+---+")
-        print(" O    |")
-        print("/|\   |")
-        print("/ \   |")
-        print("   ===")
-
-def guess_the_letter(guessedletter):
-    counter = 0
-    correct_letter = 0   # correct letters guessed
-    for guess in random_Word:
-        if(guess in guessedletter):
-            print(random_Word[counter], end=" ")
-            correct_letter += 1
-        else: 
-            print("_", " ")
-            counter += 0
-    
-    return correct_letter
-
-def print_line_under_word():
-    print("\r")
 
