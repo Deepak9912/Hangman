e302f738c16892614d262196081323009e1f908f
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-02-20 20:31:44 +0000

    add another variable random word list

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import random
 words = ["announce", "size", "amusement", "safe", "dynamic", "shaky"]
 
-def get_randomword():
+def get_randomword(words):
     """
     the function gets random words from the word list
     """
@@ -21,10 +21,10 @@ def hangman():
     letters_selected_by_user = set() # it will keep a track of letter selected by the user
       
     while len(guessed_letters) > 0:
-        print("you have used the letters: ", " ".join(letters_selected_by_user))
+        print('letters guessed: ', ' '.join(letters_selected_by_user))
 
-        random_word_list = [letter if letter in guessed_letters else "-" for letter in random_word]
-        print("current word is: ", " ".join(random_word_list))
+        random_word_list = [letter if letter in letters_selected_by_user else "-" for letter in random_word]
+        print('current word: ', ' '.join(random_word_list))
         
         #user input
         user_input_letter = input("guess a letter: ").upper() 
@@ -32,58 +32,6 @@ def hangman():
         if user_input_letter in guessed_letters:
             user_input_letter.add(guessed_letters)
         else:
-            user_input_letter.add(letters_selected_by_user)
+            user_input_letter.add(letters_selected_by_user())
 
-
-
-# getting an input from the user
-get_user_input = input("Make a guess...")
-print(get_user_input)
-
-
-
-def print_hangman():
-    if incorrect_answer == 0:
-        print("\n+===+")
-        print("      |")
-        print("      |")
-        print("      |")
-        print("   ===+")
-    if incorrect_answer == 1:
-        print("\n+===+")
-        print(" O    |")
-        print("      |")
-        print("      |")
-        print("   ===+")
-    if incorrect_answer == 2:
-        print("\n+===+")
-        print(" O    |")
-        print(" |    |")
-        print("      |")
-        print("   ===+")
-    if incorrect_answer == 3:
-        print("\n+===+")
-        print(" O    |")
-        print("/|    |")
-        print("      |")
-        print("   ===+")
-    if incorrect_answer == 4:
-        print("\n+===+")
-        print(" O    |")
-        print("/|\   |")
-        print("      |")
-        print("   ===+")
-    if incorrect_answer == 5:
-        print("\n+===+")
-        print(" O    |")
-        print("/|\   |")
-        print("/     |")
-        print("   ===+")
-    if incorrect_answer == 6:
-        print("\n+===+")
-        print(" O    |")
-        print("/|\   |")
-        print("/ \   |")
-        print("   ===+")
-
-
+hangman()
