f0089713dfa90bad42ac70a3ef81044e7e2272c8
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-02-20 14:22:00 +0000

    add while loop on if statement to show used letters to user

--- run.py
+++ run.py
@@ -19,16 +19,22 @@ def hangman():
     random_word = get_randomword(words)
     guessed_letters = set(random_word)  # it will keep a track of all the letters in the random word
     letters_selected_by_user = set() # it will keep a track of letter selected by the user
+      
+    while len(guessed_letters) > 0:
+        print("you have used the letters: ", " ".join(letters_selected_by_user))
 
-    #user input
-    user_input_letter = input("guess a letter: ").upper() 
+        random_word_list = [letter if letter in guessed_letters else "-" for letter in random_word]
+        print("current word is: ", " ".join(random_word_list))
+        
+        #user input
+        user_input_letter = input("guess a letter: ").upper() 
 
-    if user_input_letter in guessed_letters:
-        user_input_letter.add(guessed_letters)
-    elif:
-        user_input_letter.add(letters_selected_by_user)
-    else:
-        print("invalid character, please try again...")
+        if user_input_letter in guessed_letters:
+            user_input_letter.add(guessed_letters)
+        elif:
+            user_input_letter.add(letters_selected_by_user)
+        else:
+            print("invalid character, please try again...")
 
 
 
