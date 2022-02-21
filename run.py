dc90caf0ff0feaefcd2f17539af4d6691126f32e
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2022-02-21 20:32:33 +0000

    update if else statement abd break the loop

--- run.py
+++ run.py
@@ -14,22 +14,34 @@ def update_correctguess_list():
     it will print out the correct letters in the correct_guess list
     """
     for i in correct_guess:
-        print(i, end = ' ')
+        print(i, end = ' ') # end = ' ' will print the letters horizontally
     print()
 
 update_correctguess_list()
+
 
 while True:
 
     user_input = input("type a letter: ")
     print("=====================================")
 
-    if user_input in random_word:
-        correct_guess.append(user_input)
-        print("Correct guess ", correct_guess)
+    if user_input in random_word: # this if statement will allow to update the correct guess letter at correct index
+        index = 0
+        for i in random_word:
+            if i == user_input:
+                correct_guess[index] = user_input
+            index += 1
+        update_correctguess_list()
+
     else:
-        incorrect_guess.append(user_input)
-        print("Incorrect guess ", incorrect_guess)
+        if user_input not in incorrect_guess:
+            incorrect_guess.append(user_input)
+        else:
+            print("You already guessed it, please try again...")
+        print(incorrect_guess)
+    if '_' not in correct_guess:
+        print("Congratulations!!!, you have guessed the correct letter")
+        break
 
 
 
